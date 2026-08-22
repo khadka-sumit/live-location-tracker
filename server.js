bffff73ec54a2c18c34f67ab7a3a14010608000d
khadka-sumit <sumitkhadka708@gmail.com>
2026-08-22 22:08:45 +0545

    Update project configuration

--- server.js
+++ server.js
@@ -1,34 +1,35 @@
-const { tunnel: cloudflaredTunnel } = require("cloudflared")
-const cookieParser = require("cookie-parser")
-const socketIO = require("socket.io")
-const config = require("./config")
-const express = require("express")
-const tarkine = require("tarkine")
-const http = require('http')
+const cookieParser = require("cookie-parser");
+const socketIO = require("socket.io");
+const config = require("./config");
+const express = require("express");
+const tarkine = require("tarkine");
+const http = require("http");
 
-const app = express()
-const server = http.createServer(app)
-const io = new socketIO.Server(server)
-const PORT = process.env.PORT || config.port
-global.remoteURL
+const app = express();
+const server = http.createServer(app);
 
-global.IO = io
+const io = new socketIO.Server(server);
 
-app.set("view engine", "html")
-app.engine("html", tarkine.renderFile)
-app.use(cookieParser())
-app.use(express.urlencoded({ extended: false }))
-app.use(express.static(__dirname + "/public"))
-app.use(express.json())
+const PORT = process.env.PORT || config.port;
 
-app.use("/", require("./router"))
+global.IO = io;
 
-server.listen(PORT, async () => {
-    const localURL = `http://localhost:${PORT}`
-    remoteURL = await cloudflaredTunnel({
-        "--url": localURL
-    }).url
+// Local URL is used by the router/views
+global.remoteURL = `http://localhost:${PORT}`;
 
-    console.log(`LOCAL  : ${localURL}`)
-    console.log(`REMOTE : ${remoteURL}`)
-})
+app.set("view engine", "html");
+app.engine("html", tarkine.renderFile);
+
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+
+app.use("/", require("./router"));
+
+server.listen(PORT, () => {
+  console.log("--------------------------------");
+  console.log("Server started successfully");
+  console.log(`LOCAL: http://localhost:${PORT}`);
+  console.log("--------------------------------");
+});
