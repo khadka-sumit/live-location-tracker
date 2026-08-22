50e383f0d614a6a662c22993ec63e54d0a524182
khadka-sumit <sumitkhadka708@gmail.com>
2026-08-22 22:43:09 +0545

    Move credentials to environment variables

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
+
 module.exports = {
-    port: 6589,
-    username: "sumit123",
-    password: "sumitt123",
-    token: "a843def813b55c20941bdee60856b556287dc647c98f8ca036d7f95b1d0adcaa"
-}
+  port: process.env.PORT || 6589,
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  token: process.env.TOKEN,
+};
